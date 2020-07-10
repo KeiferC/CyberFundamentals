d8f58ff2cfdfbc577d33fcae42cc0f4b1c527640
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2020-07-10 11:57:26 -0400

    set up node server

--- 11-idm/saml-demo/index.js
+++ 11-idm/saml-demo/index.js
@@ -3,3 +3,31 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+
+// Express App Instance
+var app = express();
+
+app.use(cookieParser()); // for determining if user is authenticated or not
+
+app.use(bodyParser.urlencoded({extended: false}));
+    // parses `application/x-www/form-urlencoded`
+app.use(bodyParser.json()); // parses JSON
+
+app.use(session({secret: 'secret', // used for signing sessionID cookie
+                 resave: false,
+                 saveUnitialized: true}));
+    // session setup
+
+// RESTful API
+app.get('/', 
+    function (req, res) {
+        res.send('SAML Demo - SP Home Page');
+    }
+);
+
+// Server Setup
+var server = app.listen(4300, 
+    function () {
+        console.log('Listening on port %d', server.address().port);
+    }
+);
